a2849d58d9bd27f91da4d3f604633ee29270f457
sdrausty <sdrausty@gmail.com>
2017-11-14 02:42:55 +0000

    On branch master

--- setupTermuxArch.sh
+++ setupTermuxArch.sh
@@ -5,17 +5,14 @@
 # If you are encountering issues with the system-image.tar.gz file regarding download time, repository website connection and/or md5 checksum error, edit this script and change $mirror to your desired geographic location in knownconfigurations.sh.  Before editing this file, ensure termux-wake-lock is running during script operation and that you have a stable Internet connection. 
 ################################################################################
 
-printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n 🕛 \033[36;1m< 🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be enjoying the bash prompt in Arch Linux in Termux on your smartphone or tablet.  If you do not see 🕐 one o'clock below after updating Termux and installing the required components for Arch Linux in Termux installation is completed, run this script again. You might want to check your Internet connection too.  \033[37;1m\n\n	1)	Updating Termux.  \n\n	2)	Installing the script components for Arch Linux in Termux installation.  \n\n	3)	Activating termux-wake-lock.  \n\n		__This will take some time.__  \033[0m\n\n"
+printf '\033]2;  Thank you for using `setupTermuxArch.sh` 📲 \007'"\n 🕛 \033[36;1m< 🕛 This setup script will attempt to set Arch Linux up in your Termux environment.  When successfully completed, you will be enjoying the bash prompt in Arch Linux in Termux on your smartphone or tablet.  If you do not see 🕐 one o'clock below after updating Termux and installing the required components for Arch Linux in Termux installation is completed, run this script again. You might want to check your Internet connection too.  \033[37;1m\n\n	1)	Updating Termux.  \n\n	2)	Installing the script components for Arch Linux in Termux installation.  \n\n	3)	Activating termux-wake-lock.  \033[0m\n\n"
 
 if [ ! -f $PREFIX/bin/proot ]; then
-apt-get -qq update && apt-get -qq upgrade --yes
-apt-get -qq install bsdtar proot wget --yes 
+	update
 elif [ ! -f $PREFIX/bin/bsdtar ]; then
-apt-get -qq update && apt-get -qq upgrade --yes
-apt-get -qq install bsdtar proot wget --yes 
+	update
 elif [ ! -f $PREFIX/bin/wget ]; then
-apt-get -qq update && apt-get -qq upgrade --yes
-apt-get -qq install bsdtar proot wget --yes 
+	update
 else
 	printf
 fi
@@ -32,6 +29,12 @@ wget -q -N --show-progress https://raw.githubusercontent.com/sdrausty/TermuxArch
 . ./knownconfigurations.sh
 . ./necessaryfunctions.sh
 . ./printoutstatements.sh
+}
+
+update ()
+{
+	apt-get -qq update && apt-get -qq upgrade --yes
+	apt-get -qq install bsdtar proot wget --yes 
 }
 
 # Main Block
