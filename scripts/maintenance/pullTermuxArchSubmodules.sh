bc9ae2b6b4159d369085c0ce55e217b363c7835a
sdrausty <sdrausty@gmail.com>
2017-11-17 05:59:25 +0000

    On branch master

--- scripts/maintenance/pullTermuxArchSubmodules.sh
+++ scripts/maintenance/pullTermuxArchSubmodules.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/sh -e 
+#!/bin/sh -e 
 # Copyright 2017 (c) all rights reserved 
 # by S D Rausty https://sdrausty.github.io
 # Update repository and update submodules.
