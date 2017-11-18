a711e39f961536c0e51e17333bd4471e6a95a274
sdrausty <sdrausty@gmail.com>
2017-11-18 07:32:17 +0000

    On branch master

--- scripts/files/necessaryfunctions.sh
+++ scripts/files/necessaryfunctions.sh
@@ -30,13 +30,13 @@ copybin2path ()
 	read -p "Copy \`$bin\` to your \`\$PATH\`? [y|n]" answer
 	if [[ $answer = [Yy]* ]];then
 		cp $HOME/arch/$bin $PREFIX/bin
-		printf "\n 🕦 \033[36;1m<\033[0m 🕛 Copied \033[32;1m$bin\033[0m to \033[1;34m$PREFIX/bin\033[0m.  "
+		printf "\n 🕦 \033[36;1m<\033[0m 🕛 Copied \033[32;1m$bin\033[0m to \033[1;34m$PREFIX/bin\033[0m.\n\n"
 		break
 	elif [[ $answer = [Nn]* ]];then
-		printf "\n"
+		printf "\n\n"
 		break
 	elif [[ $answer = [Qq]* ]];then
-		printf "\n"
+		printf "\n\n"
 		break
 	else
 		printf "\n 🕚 \033[36;1m<\033[0m 🕛 You answered \033[33;1m$answer\033[0m.\n"
