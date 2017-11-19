b0fa24726964beba558d97627737c99e0be84203
sdrausty <sdrausty@gmail.com>
2017-11-19 02:52:12 +0000

    On branch master

--- scripts/maintenance/makedownload.sh
+++ scripts/maintenance/makedownload.sh
@@ -2,16 +2,11 @@
 # Copyright 2017 by SDRausty. All rights reserved.
 # Website for this project at https://sdrausty.github.io/TermuxArch
 # See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You 
-################################################################################
+#############################################################################
 
-if [[ $(echo $PWD |grep scripts) ]] && [[ $(echo $PWD |grep files) ]]  && [[ $(echo $PWD |grep TermuxArch) ]] ;then
 cp setupTermuxArch.sh ../..
 md5sum *sh > termuxarchchecksum.md5 
 cd ../..
 bsdtar -czv -f setupTermuxArch.tar.gz --strip-components 2 scripts/files/*
 rm scripts/files/termuxarchchecksum.md5
 md5sum setupTermuxArch.tar.gz > setupTermuxArch.md5
-else 
-	echo wrong directory $PWD
-	exit
-fi
