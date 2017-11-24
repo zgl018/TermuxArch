5e49acbe8e2f19741393a2f9a61374feb7b6ef27
sdrausty <sdrausty@gmail.com>
2017-11-24 22:23:34 +0000

    On branch master

--- scripts/files/setupTermuxArch.sh
+++ scripts/files/setupTermuxArch.sh
@@ -96,8 +96,7 @@ if [[ $1 = [Dd]* ]];then
 	printf "Submit this information if you plan to open up an issue at https://github.com/sdrausty/TermuxArch/issues to improve this installation script along with a screenshot of your topic.  \n"
 	printtail
 elif [[ $1 = [Hh]* ]];then
-	printf "\n\n\033[1;34mUsage information for \033[1;32m\`setupTermuxArch.sh\`\033[1;34m:\n\n\n\033[1;33mDEBUG\033[1;34m      Run \033[1;32m\`setupTermuxArch.sh debug\` \033[1;34mto create debug information in \033[1;32m\`setupTermuxArchDebug.log\`\033[1;34m.  Post this information along with detailed information about your issue at https://github.com/sdrausty/TermuxArch/issues.  If you think screenshots will help us in resolving your issue better, then please include them in your post.\n\n\033[1;33mHELP\033[1;34m       Run \033[1;32m\`setupTermuxArch.sh help\` \033[1;34mto output this help screen.\n\n\033[1;33mINSTALL\033[1;34m    Run \033[1;32m\`setupTermuxArch.sh\`\033[1;34m without arguments in a bash shell to install Arch Linux in Termux.\n\n\033[1;33mUNINSTALL\033[1;34m  Run \033[1;32m\`setupTermuxArch.sh uninstall\` \033[1;34mto purge your Arch Linux installation from Termux.\n"
-
+	printf "\n\n\033[1;34mUsage information for \033[1;32m\`setupTermuxArch.sh\`\033[1;34m.  You can abbreviate the argument to one letter:  \n\n\033[1;33mDEBUG\033[1;34m      Run \033[1;32m\`setupTermuxArch.sh debug\` \033[1;34mto create debug information in \033[1;32m\`setupTermuxArchDebug.log\`\033[1;34m.  Post this information along with detailed information about your issue at https://github.com/sdrausty/TermuxArch/issues.  If you think screenshots will help us in resolving your issue better, then please include them in your post.\n\n\033[1;33mHELP\033[1;34m       Run \033[1;32m\`setupTermuxArch.sh help\` \033[1;34mto output this help screen.\n\n\033[1;33mINSTALL\033[1;34m    Run \033[1;32m\`setupTermuxArch.sh\`\033[1;34m without arguments in a bash shell to install Arch Linux in Termux.\n\n\033[1;33mUNINSTALL\033[1;34m  Run \033[1;32m\`setupTermuxArch.sh uninstall\` \033[1;34mto purge your Arch Linux installation from Termux.\n"
 	printtail
 elif [[ $1 = [Uu]* ]];then
 	while true; do
@@ -110,7 +109,7 @@ elif [[ $1 = [Uu]* ]];then
 	elif [[ $uanswer = [Qq]* ]];then
 		break
 	elif [[ $uanswer = [Yy]* ]];then
-	printf "\nUninstalling Arch Linux...  \n"
+	printf "\nUninstalling Arch Linux...  \033[1;32m\n"
 	if [ -e $PREFIX/bin/$bin ] ;then
 	       	rm $PREFIX/bin/$bin 
 	else 
@@ -122,10 +121,10 @@ elif [[ $1 = [Uu]* ]];then
 		find -type d -exec chmod 700 {} \; 2>/dev/null||:
 		cd ..
 		rm -rf $HOME/arch
-		printf "Uninstalling Arch Linux done.  \n"
 	else 
 		printf "Uninstalling Arch Linux, nothing to do for $HOME/arch.\n"
 	fi
+	printf "Uninstalling Arch Linux done.  \n"
 	printtail
 	else
 		printf "\nYou answered \033[33;1m$uanswer\033[1;31m.\n\nAnswer \033[32mYes\033[1;31m or No. [\033[32my\033[1;31m|n]\n"
