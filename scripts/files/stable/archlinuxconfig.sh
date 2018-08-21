c8e88d8ce5374b70f329c329357c826034a5c2a8
sdrausty <sdrausty@gmail.com>
2018-08-21 14:33:00 -0400

    Commit on 20180821 branch master 52451174875967

--- scripts/files/stable/archlinuxconfig.sh
+++ scripts/files/stable/archlinuxconfig.sh
@@ -218,7 +218,7 @@ addch() { # Creates .hushlogin and .hushlogout file
 	################################################################################
 	set -Eeou pipefail 
 	declare -a args
-versionid="v1.6 id7139"
+versionid="v1.6 id0384"
 
 
 	finishe() { # on exit
@@ -452,7 +452,7 @@ addkeys() {
 	shopt -s nullglob globstar
 
 	declare -a keyrings
-versionid="v1.6 id7139"
+versionid="v1.6 id0384"
 
 
 	finishe() { # on exit
@@ -554,7 +554,7 @@ addpc() { # pacman install packages shortcut
 	shopt -s nullglob globstar
 
 	declare -g args="\$@"
-versionid="v1.6 id7139"
+versionid="v1.6 id0384"
 
 
 	finishe() { # on exit
@@ -617,7 +617,7 @@ addpci() { # system update with pacman install packages shortcut
 	shopt -s nullglob globstar
 
 	declare args="\$@"
-versionid="v1.6 id7139"
+versionid="v1.6 id0384"
 
 
 	finishe() { # on exit
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="v1.6 id7139"
+versionid="v1.6 id0384"
 
 ## Init Functions ##############################################################
 
