3b854dcc1b964a30d89ad3f677ad8bff44a4d9f1
sdrausty <sdrausty@gmail.com>
2018-08-22 10:51:25 -0400

    Commit on 20180822 branch master 42797897442598

--- scripts/files/working/archlinuxconfig.sh
+++ scripts/files/working/archlinuxconfig.sh
@@ -218,7 +218,7 @@ addch() { # Creates .hushlogin and .hushlogout file
 	################################################################################
 	set -Eeou pipefail 
 	declare -a args
-versionid="gen.v1.6 id291614446426"
+versionid="v1.6 id2836"
 
 
 	finishe() { # on exit
@@ -484,7 +484,7 @@ addkeys() {
 	shopt -s nullglob globstar
 
 	declare -a keyrings
-versionid="gen.v1.6 id291614446426"
+versionid="v1.6 id2836"
 
 
 	finishe() { # on exit
@@ -586,7 +586,7 @@ addpc() { # pacman install packages shortcut
 	shopt -s nullglob globstar
 
 	declare -g args="\$@"
-versionid="gen.v1.6 id291614446426"
+versionid="v1.6 id2836"
 
 
 	finishe() { # on exit
@@ -649,7 +649,7 @@ addpci() { # system update with pacman install packages shortcut
 	shopt -s nullglob globstar
 
 	declare args="\$@"
-versionid="gen.v1.6 id291614446426"
+versionid="v1.6 id2836"
 
 
 	finishe() { # on exit
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6 id291614446426"
+versionid="v1.6 id2836"
 
 ## Init Functions ##############################################################
 
