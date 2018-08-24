d42703cb5c37535dfec4957ed052154512349450
sdrausty <sdrausty@gmail.com>
2018-08-24 15:06:30 -0400

    Commit on 20180824 branch master 22489642594420

--- scripts/files/stable/maintenanceroutines.sh
+++ scripts/files/stable/maintenanceroutines.sh
@@ -54,12 +54,12 @@ copyimage() { # A systemimage.tar.gz file can be used: `setupTermuxArch.sh ./[pa
 	declare idir="$PWD"
 	cfile="${1##/*/}" 
  	file="$cfile" 
-	if [[ "$lc" = "" ]];then
+	if [[ "$lcc" = "" ]];then
 		echo "Copying $1.md5…" 
 		cp "${wdir}$1".md5  "$installdir"
 		echo "Copying $1…" 
 		cp "${wdir}$1" "$installdir"
-	elif [[ "$lc" = "1" ]];then
+	elif [[ "$lcc" = "1" ]];then
 		echo "Copying $1.md5…" 
 		cp "$idir/$cfile".md5  "$installdir" 
 		echo "Copying $1…" 
