e870898a941b0fc41d2680f20c2a8a491b70b337
sdrausty <sdrausty@gmail.com>
2018-08-25 09:20:51 -0400

    Commit on 20180825 branch master 766213437868

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6 id367613174960"
+versionid="gen.v1.6 id766213437868"
 
 ## Init Functions ###################################################################################################################################
 
@@ -517,7 +517,7 @@ declare cpuabi8="arm64-v8a"
 declare cpuabix86="x86"
 declare cpuabix86_64="x86_64"
 declare dfl="/gen" # Used for development 
-declare dm=""  ## download manager
+declare dm="wget"  ## download manager
 declare dmverbose="-q" # -v for verbose download manager output from curl and wget;  for verbose output throughout runtime also change in `setupTermuxArchConfigs.sh` when using `setupTermuxArch.sh manual`. 
 declare	ed=""
 declare installdir=""
