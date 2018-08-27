d21b92a956ae6fbb9608a44136bba647ffb4d92b
sdrausty <sdrausty@gmail.com>
2018-08-26 20:51:34 -0400

    Commit on 20180826 branch master 858088842093

--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="gen.v1.6 id800098254698"
+versionid="gen.v1.6 id858088842093"
 
 ## Init Functions ###################################################################################################################################
 
