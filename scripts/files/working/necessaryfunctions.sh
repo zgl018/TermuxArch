ea4477b4b06386af148c19fe2134e2f560c22496
sdrausty <sdrausty@gmail.com>
2018-08-29 11:31:47 -0400

    Commit on 20180829 branch master 162777804625

--- scripts/files/working/necessaryfunctions.sh
+++ scripts/files/working/necessaryfunctions.sh
@@ -126,7 +126,7 @@ makefinishsetup() {
 	################################################################################
  	set -Eeou pipefail 
 	shopt -s nullglob globstar
-versionid="v1.6 id3991"
+versionid="gen.v1.6 id162777804625"
 	printf "\\n\\e[1;34m:: \\e[1;37mRemoving redundant packages for Termux PRoot installation…\\n"
 	EOM
 	if [[ -e "$HOME"/.bash_profile ]];then
@@ -177,7 +177,7 @@ makesetupbin() {
 	################################################################################
  	set -Eeou pipefail 
 	shopt -s nullglob globstar
-versionid="v1.6 id3991"
+versionid="gen.v1.6 id162777804625"
 	unset LD_PRELOAD
 	EOM
 	echo "$prootstmnt /root/bin/finishsetup.sh ||:" >> root/bin/setupbin.sh 
@@ -194,7 +194,7 @@ makestartbin() {
 	################################################################################
 	set -Eeou pipefail 
 	shopt -s nullglob globstar
-versionid="v1.6 id3991"
+versionid="gen.v1.6 id162777804625"
 	unset LD_PRELOAD
 	declare -g ar2ar="\${@:2}"
 	declare -g ar3ar="\${@:3}"
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="v1.6 id3991"
+versionid="gen.v1.6 id162777804625"
 ## Init Functions ##############################################################
 
 aria2cif() { 
