00da636ef2c98225bd29b193251febd8952f802a
sdrausty <sdrausty@gmail.com>
2018-09-01 16:15:09 -0400

    Commit on 20180901 branch master 48974374286872

--- scripts/files/working/archlinuxconfig.sh
+++ scripts/files/working/archlinuxconfig.sh
@@ -60,9 +60,10 @@ addbash_profile() {
 	. "\$HOME"/.bashrc
 	PS1="[\A\[\033[0;32m\] \W \[\033[0m\]]\\$ "
 	export TZ="$(getprop persist.sys.timezone)"
-	export LANG="$_LANGUAGE" 
-	export LANGUAGE="$_LANGUAGE" 
 	EOM
+	for i in "${!_LC_TYPE[@]}"; do
+	 	printf "%s=\"%s\"\\n" "export ${_LC_TYPE[i]}" "$_LANGUAGE.UTF-8" >> root/.bash_profile 
+	done
 	if [ -e "$HOME"/.bash_profile ] ; then
 		grep proxy "$HOME"/.bash_profile |grep "export" >>  root/.bash_profile 2>/dev/null ||:
 	fi
@@ -130,7 +131,7 @@ addch() {
 	callfileheader root/bin/ch "# Creates .hushlogin and .hushlogout file"
 	cat >> root/bin/ch <<- EOM
 	declare -a args
-versionid="gen.v1.6 id624090994607"
+versionid="v1.6 id3556"
 
 	finishe() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -352,7 +353,7 @@ addkeys() {
 	callfileheader root/bin/keys 
 	cat >> root/bin/keys <<- EOM
 	declare -a keyrings
-versionid="gen.v1.6 id624090994607"
+versionid="v1.6 id3556"
 
 	finishe() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -445,7 +446,7 @@ addpc() {
 	callfileheader root/bin/pc "# Pacman install packages wrapper without system update."
 	cat >> root/bin/pc  <<- EOM
 	declare -g args="\$@"
-versionid="gen.v1.6 id624090994607"
+versionid="v1.6 id3556"
 
 	finishe() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -499,7 +500,7 @@ addpci() {
 	callfileheader root/bin/pci "# Pacman install packages wrapper with system update."
 	cat >> root/bin/pci  <<- EOM
 	declare args="\$@"
-versionid="gen.v1.6 id624090994607"
+versionid="v1.6 id3556"
 
 	finishe() { # on exit
 		printf "\\e[?25h\\e[0m"
