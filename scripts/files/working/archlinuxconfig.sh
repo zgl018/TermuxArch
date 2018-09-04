7df57179ff1b4f4a4eba0f74fa332a5731d426e0
sdrausty <sdrausty@gmail.com>
2018-09-03 20:11:40 -0400

    Commit on 20180903 branch master 285810340650

--- scripts/files/working/archlinuxconfig.sh
+++ scripts/files/working/archlinuxconfig.sh
@@ -132,7 +132,7 @@ addch() {
 	_CFLHDR_ root/bin/ch "# Creates .hushlogin and .hushlogout file"
 	cat >> root/bin/ch <<- EOM
 	declare -a args
-versionid="v1.6 id4682"
+versionid="gen.v1.6 id285810340650"
 
 	_TRPET_() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -347,7 +347,7 @@ addkeys() {
 	_CFLHDR_ root/bin/keys 
 	cat >> root/bin/keys <<- EOM
 	declare -a keyrings
-versionid="v1.6 id4682"
+versionid="gen.v1.6 id285810340650"
 
 	_TRPET_() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -423,7 +423,7 @@ addpc() {
 	_CFLHDR_ root/bin/pc "# Pacman install packages wrapper without system update."
 	cat >> root/bin/pc  <<- EOM
 	declare -g args="\$@"
-versionid="v1.6 id4682"
+versionid="gen.v1.6 id285810340650"
 
 	_TRPET_() { # on exit
 		printf "\\e[?25h\\e[0m"
@@ -460,7 +460,7 @@ addpci() {
 	_CFLHDR_ root/bin/pci "# Pacman install packages wrapper with system update."
 	cat >> root/bin/pci  <<- EOM
 	declare args="\$@"
-versionid="v1.6 id4682"
+versionid="gen.v1.6 id285810340650"
 
 	_TRPET_() { # on exit
 		printf "\\e[?25h\\e[0m"
--- scripts/files/working/setupTermuxArch.sh
+++ scripts/files/working/setupTermuxArch.sh
@@ -9,7 +9,7 @@ IFS=$'\n\t'
 set -Eeuo pipefail
 shopt -s nullglob globstar
 unset LD_PRELOAD
-versionid="v1.6 id4682"
+versionid="gen.v1.6 id285810340650"
 ## INIT FUNCTIONS ##############################################################
 aria2cif() { 
 	dm=aria2c
